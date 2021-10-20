05c1b4c276397cac41aceea0a93b7774d8212aca
Andrey Kruglov <akruglov@metainfo.ru>
2021-10-20 21:42:30 +0300

    Correct binding

---
+++ WpfApp1/GraphData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace WpfApp1
+{
+    public class GraphData
+    {
+        public SeriesCollection SeriesCollection { set; get; }
+
+        public GraphData()
+        {
+            this.LoadData();
+        }
+
+        private void LoadData()
+        {
+            const int reportId = 16;
+
+            var data = this.LoadDataFromServer(reportId);
+
+            this.SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double>(data.Take(4)),
+                },
+                new LineSeries
+                {
+                    Values = new ChartValues<double>(data.Skip(4).Take(4)),
+                },
+            };
+        }
+
+        // Тут чтение данных из базы, какой-нибудь List<ResultItem>
+        private List<double> LoadDataFromServer(int reportId)
+        {
+            return new List<double> { 1, 3, 5, 7, 8, 6, 2, 4 };
+        }
+    }
+}
--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,8 +9,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using LiveCharts;
-using LiveCharts.Wpf;
 
 namespace WpfApp1
 {
@@ -24,42 +20,6 @@ namespace WpfApp1
         public Window1()
         {
             InitializeComponent();
-            this.DataContext = new GraphData();
-        }
-    }
-
-    public class GraphData
-    {
-        public SeriesCollection SeriesCollection { set; get; }
-
-        public GraphData()
-        {
-            this.LoadData();
-        }
-
-        private void LoadData()
-        {
-            const int reportId = 16;
-
-            var data = LoadDataFromServer(reportId);
-
-            this.SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Values = new ChartValues<double>(data.Take(4)),
-                },
-                new LineSeries
-                {
-                    Values = new ChartValues<double>(data.Skip(4).Take(4)),
-                },
-            };
-        }
-
-        // Тут чтение данных из базы, какой-нибудь List<ResultItem>
-        private List<double> LoadDataFromServer(int reportId)
-        {
-            return new List<double> { 1, 3, 5, 7, 8, 6, 2, 4 };
         }
     }
 }
